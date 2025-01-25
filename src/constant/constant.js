62ceb8544771d7c28d3287166446cf49b9e78d4c
zaibali <zaibali51@live.com>
2025-01-25 19:15:04 +0500

    update production URL to point to the deployed backend service

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,9 +1,9 @@
 
 const DEV_URL = `http://localhost:3000/`
-const PROD_URL = `http://localhost:3000/`
+const PROD_URL = `https://hackathon-backend-kappa.vercel.app`
 
 
-export const BASE_URL = DEV_URL
+export const BASE_URL = PROD_URL
 
 export const ApiRoutes = {
     login: BASE_URL + 'auth/login',
